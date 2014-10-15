4593796d5f2f47960e042d334e48ec2373a9d550
Nicolai Reigstad <Nicolai887@gmail.com>
2014-10-15 14:18:15 +0200

    no message

---
+++ RPGfirstTry/Assets/rotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour {
+
+    public float rotationSpeed = 100.0f;
+    public float moveSpeed = 14.0f;
+    public float acceleration = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //transform.position = transform.position + (moveSpeed * Vector3.right) * Time.deltaTime;
+        //gameObject.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.World);
+
+        transform.Translate(acceleration * Time.deltaTime, 0, 0);
+        if (acceleration < moveSpeed)
+        {
+            acceleration += 0.5f;
+        }
+	}
+}   
